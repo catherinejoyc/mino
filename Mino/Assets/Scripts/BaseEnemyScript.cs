4f37c59d084518968e4552d4d34f84c599679d8e
Cat <cal17425@spengergasse.at>
2018-12-06 15:19:10 +0100

    EnemyScript angefangen

---
+++ Mino/Assets/Scripts/BaseEnemyScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BaseEnemyScript : MonoBehaviour {
+
+    NavMeshAgent m_agent;
+    GameObject m_player;
+    float m_maxDistanceToPlayer;
+
+	// Use this for initialization
+	void Start () {
+        m_agent = GetComponent<NavMeshAgent>();
+        m_maxDistanceToPlayer = GetComponent<SphereCollider>().radius;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TrackPlayer() //Track player if no wall is inbetween
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + transform.up * 0.5f, m_player.transform.position - transform.position, out hit, m_maxDistanceToPlayer))
+        {
+            if (hit.collider.name == "Player")
+            {
+                //m_agent.SetDestination(m_player);
+            }
+        }
+    }
+}
--- Mino/Assets/Scripts/PlayerSoundScript.cs
+++ Mino/Assets/Scripts/PlayerSoundScript.cs
@@ -5,7 +5,9 @@ using UnityEngine.Events;
 
 public class PlayerSoundScript : MonoBehaviour {
 
-    // https://docs.unity3d.com/ScriptReference/Events.UnityEvent.AddListener.html
+    PlayerController m_player;
+    public float stepIntervall;
+    float lastStepTime;
 
     /* Laute Geräusche:
      * Laufen
@@ -14,7 +16,7 @@ public class PlayerSoundScript : MonoBehaviour {
      * Kisten/Leitern platzieren
      * Rascheln im Gebüsch/andere Pflanzen)
     */
-    UnityEvent m_loudSound = new UnityEvent();
+    UnityEvent m_PlayRunningFootstep = new UnityEvent();
    
 
     /* Leise Geräusche:
@@ -22,11 +24,36 @@ public class PlayerSoundScript : MonoBehaviour {
      * ---
      * Steinchen aufprallen (von der Decke und vom Spieler geworfen)
     */
-    UnityEvent m_quietSound = new UnityEvent();
+    UnityEvent m_PlaySneakingFootstep = new UnityEvent();
+
+    private void Awake()
+    {
+        //Add PlayAudio to Events
+        m_PlayRunningFootstep.AddListener(PlayRunningFootstep);
+        m_PlaySneakingFootstep.AddListener(PlaySneakingFootstep);
+    }
 
     private void Update()
     {
-        //play Footstep every frame
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            //play Footstep every few sec
+            if (Time.time > lastStepTime + stepIntervall)
+            {
+                //Play Events
+                m_PlayRunningFootstep.Invoke();
 
+                lastStepTime = Time.time;
+            }
+        }
+    }
+
+    void PlayRunningFootstep()
+    {
+        print("Footstep");
+    }
+    void PlaySneakingFootstep()
+    {
+        print("Sneak");
     }
 }
