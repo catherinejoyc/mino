e0f28467989114a8fbbee0c7a238afd5d0c451bd
Cat <cal17425@spengergasse.at>
2019-03-01 13:31:11 +0100

    Headphone Scene is built

---
+++ Mino/Assets/Scripts/HeadphoneSceneScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HeadphoneSceneScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Press Any Key to continue#
+        if (Input.anyKey)
+        {
+            int lvl = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(lvl+1);
+        }
+	}
+}
