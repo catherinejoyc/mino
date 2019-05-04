b4f9bafc1b803e2b483e0c35f0202bf2694900dd
Cat <cal17425@spengergasse.at>
2019-05-04 23:48:39 +0200

    Schlüsselfragment Tutorial Screen bearbeitet

---
+++ Mino/Assets/Scripts/Special Occasions/DestroyAfterTriggerEnter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTriggerEnter : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+            this.gameObject.SetActive(false);
+    }
+}
