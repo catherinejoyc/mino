e897dd7c0c9c631c82e1b048cb1f7e2b8c462e46
Diem Noah <DIE16757@spengergasse.at>
2019-05-04 18:06:47 +0200

    Sounds für alle Menue screens fertig

---
+++ Mino/Assets/Scripts/UISounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UISounds : MonoBehaviour {
+
+    public AK.Wwise.Event playPositiveEvent;
+    public AK.Wwise.Event playNegativeEvent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void PlayPosUISound()
+    {
+        playPositiveEvent.Post(this.gameObject);
+    }
+    public void PlayNegUISound()
+    {
+        playNegativeEvent.Post(this.gameObject);
+    }
+}
